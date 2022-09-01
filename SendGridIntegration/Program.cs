c2af40ca5a8b9f766002a35407a87ddd75468aaa
Chelsea <chelsea@tripwiretech.com>
2022-09-01 17:06:10 +1200

    Sendgrid integration

---
+++ SendGridIntegration/Program.cs
@@ -0,0 +1,17 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+static async Task Execute()
+{
+    var apiKey = "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";
+    var client = new SendGridClient(apiKey);
+    var from = new EmailAddress("chelsea@tripwiretech.com", "Chelsea");
+    var subject = "My Subject - Hi YouTube!";
+    var to = new EmailAddress("hello@tripwiretech.com", "Hello");
+    var plainTextContent = "How cool an email!";
+    var htmlContent = "<strong>How cool an email!</strong>";
+    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+    var response = await client.SendEmailAsync(msg);
+}
+
+await Execute();
